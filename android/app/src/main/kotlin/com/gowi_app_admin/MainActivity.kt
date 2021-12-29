98b104621357f3c5c5470a02efe127275b6871fd
git <ali.khalifa@oasisoft.net>
2021-12-29 00:50:36 -0800

    first

---
+++ android/app/src/main/kotlin/com/gowi_app_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gowi_app_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:gowi_app_admin/account/login.dart';
+import 'package:gowi_app_admin/home/home.dart';
+import 'package:gowi_app_admin/provider/loading.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'config.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
+  G_use_id_val = prefs!.getString(G_use_id);
+  runApp(Splash());
+}
+
+//343434
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<LoadingControl>(
+            create: (context) => LoadingControl(),
+          )
+        ],
+        child: MaterialApp(
+            theme: ThemeData(fontFamily: 'GE_ar'),
+            debugShowCheckedModeBanner: false,
+            home: new SplashScreen(
+              seconds: 3,
+              routeName: "/",
+              navigateAfterSeconds:
+              G_use_id_val == null ? new Login() : new Home(),
+              title: new Text(
+                'مرحبا بكم في تطبيق ادارة المطعم',
+                style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.white),
+              ),
+              /*image: new Image.network(
+          'https://flutter.io/images/catalog-widget-placeholder.png'),*/
+              backgroundColor: primaryColor,
+              styleTextUnderTheLoader: new TextStyle(),
+              photoSize: 100.0,
+              onClick: () => print("restaurant"),
+              loaderColor: Colors.white,
+            )));
+  }
+}
